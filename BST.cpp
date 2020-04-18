dadbd020173eadc65039ac32af723dc05364f394
Riftrecker <55419291+Beetman123@users.noreply.github.com>
2020-04-18 12:41:29 -0700

    Last save before getting rid of struct

--- BST.cpp
+++ BST.cpp
@@ -15,33 +15,33 @@ BST::BST()
 // error is that none of it is saved !!!!!!!!!!!!!!!!!
 
 // add a node to the BST
-void BST::addNode(const int data)
-{
-	// if empty
-	if (head == nullptr)
-	{
-		head = &newNode(data);
-		curr = head; // so to get rid of an error
-	}
-
-	// if current is empty
-	else if (curr == nullptr) // never got hit (curr was undefined but not nullptr)
-	{
-		curr = &newNode(data);
-	}
-
-	// if data is smaller then current
-	else if (data < curr->data) // EXCEPTION (this->curr was 0xCCCCCC) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!(bigger problem at moment)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	{
-		curr = curr->left;
-	}
-
-	// if data is larger then current
-	else if (data > curr->data)
-	{
-		curr = curr->right;
-	}
-}
+//void BST::addNode(const int data)
+//{
+//	// if empty
+//	if (head == nullptr)
+//	{
+//		head = &newNode(data);
+//		curr = head; // so to get rid of an error
+//	}
+//
+//	// if current is empty
+//	else if (curr == nullptr) // never got hit (curr was undefined but not nullptr)
+//	{
+//		curr = &newNode(data);
+//	}
+//
+//	// if data is smaller then current
+//	else if (data < curr->data) // EXCEPTION (this->curr was 0xCCCCCC) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!(bigger problem at moment)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//	{
+//		curr = curr->left;
+//	}
+//
+//	// if data is larger then current
+//	else if (data > curr->data)
+//	{
+//		curr = curr->right;
+//	}
+//}
 
 /*
 void BST::addNode(const int data)
@@ -82,4 +82,32 @@ Node BST::newNode(const int data)
 	newNode.right = nullptr;
 
 	return newNode;
+}
+
+void BST::addNode(const int data, BST* current)
+{
+	// if empty
+	if (head == nullptr)
+	{
+		head = &newNode(data);
+		curr = head; // so to get rid of an error
+	}
+
+	// if current is empty
+	else if (curr == nullptr) // never got hit (curr was undefined but not nullptr)
+	{
+		curr = &newNode(data);
+	}
+
+	// if data is smaller then current
+	else if (data < curr->data) // EXCEPTION (this->curr was 0xCCCCCC) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!(bigger problem at moment)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	{
+		curr = curr->left;
+	}
+
+	// if data is larger then current
+	else if (data > curr->data)
+	{
+		curr = curr->right;
+	}
 }
